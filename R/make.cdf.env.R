5896d91fc297aba9f60397b7a51b2f4d590e02f5
dfcimm5 <unknown>
2003-03-07 02:49:41 +0000

    Added two functions xy2i() and  i2xy(), and cleaned up the package creation mechanism, by using createPackage() and template files rather than cat/paste sequences.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@3537 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/make.cdf.env.R
+++ R/make.cdf.env.R
@@ -6,8 +6,8 @@ make.cdf.env <- function(filename,cdf.path=getwd(),env.name="tmpenv",
 
   if(verbose) cat("Creating environment\n")
   ##extract number or rows and number of columns
-  nrow <- dim(cdf@name)[1]
-  ncol <- dim(cdf@name)[2]
+  sizex <- dim(cdf@name)[1]
+  sizey <- dim(cdf@name)[2]
   
   ##where are the pm and mm
   pm.or.mm <- as.vector(pmormm(cdf))
@@ -15,25 +15,27 @@ make.cdf.env <- function(filename,cdf.path=getwd(),env.name="tmpenv",
   mmindex <- !pm.or.mm
   rm(pm.or.mm)
   
-  ##get ready to go
-  ##start the new environment
-  ##position in vector will correspont to the position on the array
-  ##given ncol and nrow we can figure out the 2D (x,y) position
-  ##now we assing 
+  ## get ready to go
+  ## start the new environment
+  ## position in vector will correspond to the position on the array
+  ## given ncol and nrow we can figure out the 2D (x,y) position
+  ## now we assign 
   assign(env.name,new.env(hash=TRUE))
   genenames <- name.levels(cdf) ##so that we only use name.level method once
   n <- length(genenames) ##number of genes
   probenames <-  as.vector(cdf@name)
   position <- 1:length(probenames)
   numbers <- as.vector(cdf@atom)##number relates to position in genome
-  tmp<- split(c(position,pmindex,mmindex,numbers),
+  tmp<- split(c(position,  pmindex,   mmindex,   numbers),
               c(probenames,probenames,probenames,probenames))
   rm(pmindex,mmindex,position,numbers,probenames)
   names(tmp) <- genenames[as.numeric(names(tmp))]
   ## this list will be converted to an environment using multiassign
-  if(verbose) cat("Wait for about",round(length(tmp)/100),"dots")
+  if(verbose)
+    cat("Wait for about",round(length(tmp)/100),"dots")
   tmp <- lapply(tmp,function(x){
-    if(verbose) if(sample(1:100)==1) cat(".")
+    if(verbose)
+      if(runif(1)<0.01) cat(".")
     n <- length(x)/4
     position <- x[1:n] 
     pmindex <- which(x[(n+1):(2*n)]==1)
@@ -50,78 +52,62 @@ make.cdf.env <- function(filename,cdf.path=getwd(),env.name="tmpenv",
   })
   if(verbose) cat("\n")
   multiassign(names(tmp),tmp,get(env.name))
-  return(get(env.name))
+
+  syms = list(
+    XY2I  = paste("xy2i = function(x,y) {y*", sizex, "+x+1}", sep=""),
+    I2XY  = paste("i2xy = function(i) {cbind(i%%", sizex, "-1, i%/%", sizex, ")}", sep=""),
+    SIZEX = paste(sizex),
+    SIZEY = paste(sizey),
+    SIZEI = paste(sizex*sizey))
+  
+  return(list(env=get(env.name), syms=syms))
 }
 
-##very rough package maker. 
-make.cdf.package<- function(filename,packagename=NULL,
-                            cdf.path=getwd(),package.path=getwd(),
-                            author="The Bioconductor Project",
-                            maintainer="The Bioconductor Project <bioconductor@stat.math.ethz.ch>",
-                            version=library(help=makecdfenv)$info[[2]][[2]][2],
-                            verbose=TRUE){
-  ##if no packagename given change CDF filename to packagename
-  if(is.null(packagename)){
-    packagename <- gsub("_","",tolower(filename))
-    packagename <- gsub("\\.","",packagename)
-    packagename <- gsub("-","",packagename)
-    packagename <- gsub("\ ","",packagename)
-  }
-
-  fullpath<- paste(path.expand(package.path),packagename,sep="/")
-  if(file.exists(fullpath))
-    stop(paste(fullpath," exists. Please remove first.\n",sep=""))
+## package maker
+make.cdf.package<- function(filename,
+                            packagename = NULL,
+                            cdf.path = getwd(),
+                            package.path = getwd(),
+                            author = "The Bioconductor Project",
+                            maintainer = "The Bioconductor Project <bioconductor@stat.math.ethz.ch>",
+                            version = library(help=makecdfenv)$info[[2]][[2]][2],
+                            force   = FALSE,
+                            verbose = TRUE) {
   
-  dir.create(fullpath)
-  dir.create(paste(fullpath,"data",sep="/"))
-  dir.create(paste(fullpath,"man",sep="/"))
-  dir.create(paste(fullpath,"R",sep="/"))
+  ## If no packagename given change CDF filename to packagename.
+  ## cleancdfname() is defined in package 'affy'.
+  if(is.null(packagename))
+    packagename <- cleancdfname(filename) 
 
-  ##TITLE FILE
-  write(packagename,file=paste(fullpath,"TITLE",sep="/"))
+  cdf <- make.cdf.env(filename,cdf.path=cdf.path)
+  assign(packagename, cdf$env)
 
-  ##DESCRIPTION FILE
-  write(paste("Package: ",packagename,"\n",
-              "Title: ",packagename,"\n",
-              "Version: ",version,"\n",
-              "Created: ",date(),"\n",
-              "Author: ",author,"\n",
-              "Description: A package containing an evironmet represeting the ",filename," file.\n",
-              "Maintainer: ",maintainer,"\n",
-              "License: LGPL\n",sep=""),
-        file=paste(fullpath,"DESCRIPTION",sep="/"))
+  home  <- .path.package("makecdfenv")
+  symbols = append(cdf$syms,
+    list(PKGNAME     = packagename,
+         VERSION     = version,
+         AUTHOR      = author,
+         TODAY       = date(),
+         CDFFILENAME = filename,
+         MAINTAINER  = maintainer))
 
-  ##INDEX file
-  write(paste(packagename,"environment containing the location probe set membership mapping",sep="\t"),file=paste(fullpath,"INDEX",sep="/"))
+  ## see in the subdirectory 'Code' of this package for all the
+  ## files that go into the package!
+  res = createPackage(packagename,
+                destinationDir = path.expand(package.path),
+                originDir  = file.path(home, "Code"),
+                symbolValues = symbols,
+                force = force, quiet = !verbose)
+
+  for (fn in c("TITLE", "INDEX", "data/00Index"))
+    copySubstitute(file.path(file.path(home, "Code", fn)),
+                   file.path(res$pkgdir, fn),
+                   symbols)
   
-  ##data/00Index file
-  write(paste(packagename,"environment containing the location probe set membership mapping",sep="\t"),file=paste(fullpath,"data","00Index",sep="/"))
+  ## save an XDR file with the environment
+  save(list = packagename, file = file.path(res$pkgdir, "data", paste(packagename,".rda",sep="")))
   
-  ##zzz.R
-  write(paste(".First.lib <- function(libname, pkgname){\n",
-              "\tpath = .path.package(pkgname)\n",
-              "\twhere <- as.environment(match(paste(\"package:\", pkgname, sep = \"\"),search()))\n",
-              paste("\tload(file.path(path, \"data\", \"",
-                    paste(packagename,".rda",sep=""),
-                    "\") ,envir = where)\n",sep=""),
-              "}\n",sep=""),file=paste(fullpath,"R","zzz.R",sep="/"))
-  
-  ##Rd file
-  write(paste("\\name{",packagename,"}\n",
-              "\\alias{",packagename,"}\n",
-              "\\non_function{}\n",
-              "\\title{",packagename,"}\n",
-              "\\description{environment describind CDF file}\n",
-              "\\keyword{datasets}\n",sep=""),
-        file=paste(fullpath,"man",paste(packagename,".Rd",sep=""),sep="/"))
-  
-
-  ###the environmet
-  tmp <- make.cdf.env(filename,cdf.path=cdf.path)
-
-  assign(packagename,tmp)
-  save(list=packagename,file=paste(package.path,"/",packagename,"/data/",packagename,".rda",sep=""))
-  TRUE
+  return(TRUE)
 }
 
 
