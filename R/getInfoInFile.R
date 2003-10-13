bb7e504b6caca4431df76a80affae9ba3a36d886
dfcimm5 <unknown>
2003-10-13 10:15:51 +0000

    Added Hu6800.CDF.gz for the examples/vignette. Moved "getInfo..." from affy to makecdfenv
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@6066 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getInfoInFile.R
@@ -0,0 +1,7 @@
+getInfoInAffyFile <- function(filename, type, unit, property, compress=NULL) {
+  if (is.null(compress) && (type == "CDF"))
+    compress <- getOption("BioC")$affy$compress.cdf
+  if (is.null(compress) && (type == "CEL"))
+    compress <- getOption("BioC")$affy$compress.cel
+  .Call("getInfo", as.character(filename), as.character(type), as.character(unit), as.character(property), as.integer(compress))
+}
