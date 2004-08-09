14a815939cb3676a41ad13b31db9891de484dbed
James MacDonald <jmacdon@med.umich.edu>
2004-08-09 18:11:13 +0000

    JWM:Changed zzz.R in Code directory to accept either an .rda file or a Rdata.zip file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@9655 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Code/R/zzz.R
+++ inst/Code/R/zzz.R
@@ -1,5 +1,5 @@
 .First.lib <- function(libname, pkgname){
       path = .path.package(pkgname)
       where <- as.environment(match(paste("package:", pkgname, sep = ""),search()))
-      load(file.path(path, "data", "@PKGNAME@.rda"), envir = where)
+      data(list="@PKGNAME@", package=pkgname, envir = where)
 }
