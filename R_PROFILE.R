959567f9720eba15d7db4667e48848d631693c08
Seth Falcon <sfalcon@fhcrc.org>
2005-10-11 16:30:50 +0000

    Use SaveImage.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@14443 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R_PROFILE.R
+++
@@ -1,3 +0,0 @@
-require("utils", quietly=TRUE)
-require("affy", quietly=TRUE)
-require("methods", quietly=TRUE)
--- install.R
+++
@@ -1,5 +0,0 @@
-## affy is required for 'cleancdfname'
-## Biobase for 'multiassign'
-require("utils", quietly=TRUE)
-require("affy", quietly=TRUE) 
-require("methods", quietly=TRUE)
