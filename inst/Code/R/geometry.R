50534ddd7fadb479d10960148300e2c459a56740
James MacDonald <jmacdon@med.umich.edu>
2007-03-23 18:37:21 +0000

    Deprecated xy2i and i2xy.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@23418 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Code/R/geometry.R
+++ inst/Code/R/geometry.R
@@ -1,2 +1,12 @@
-xy2i = function(x,y) {@I2XY@}
-i2xy = function(i) {@XY2I@}
+xy2i = function(x,y) {
+    .Deprecated("xy2indices",
+                msg = paste("xy2i is deprecated and will be removed in the next BioC release.\n",
+                "Use xy2indices in the affy package instead.", sep=""))
+    @I2XY@
+}
+i2xy = function(i) {
+    .Deprecated("indices2xy",
+                msg = paste("i2xy is deprecated and will be removed in the next BioC release.\n",
+                "Use indices2xy in the affy package instead.", sep=""))
+    @XY2I@
+}
