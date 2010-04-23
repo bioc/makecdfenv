68ff19e9f0cb2dd337b195f061d8f56a91e71c67
Patrick Aboyoun <unknown>
2010-04-23 05:11:18 +0000

    Remove unneeded .First.lib function.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@46447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-##-----------------------------------------------------------------
-## .First.lib: this function is called when the package is loaded
-##-----------------------------------------------------------------
-.First.lib <- function(libname, pkgname, where) {
-  require("utils")
-  library.dynam("makecdfenv", pkgname, libname)
-}
-
