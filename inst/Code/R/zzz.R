cdb51512405a494c6bab291b171fc8d810b3e719
James MacDonald <jmacdon@med.umich.edu>
2014-02-24 15:47:28 +0000

    Various changes; importing affyio, changed code for cdf packages to import AnnotationDbi and utils and changed .onLoad to no longer require(AnnotationDbi). Also changed default maintainer and email.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/makecdfenv@86721 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/Code/R/zzz.R
+++ inst/Code/R/zzz.R
@@ -1,5 +1,4 @@
 .onLoad <- function(libname, pkgname){
-      require("AnnotationDbi", quietly=TRUE)
       where = asNamespace(pkgname)
       data(list = pkgname, package = pkgname, envir = where)
       packageStartupMessage(AnnotationDbi:::annoStartupMessages(pkgname))
